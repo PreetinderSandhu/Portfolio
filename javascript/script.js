0045c6e618d58ceed04598a5e35bdd4daa3a2d05
Preet <Preet.sandhu@yahoo.in>
2019-03-04 02:28:29 -0500

    changes on portfolio

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    if ((screen.width> 800)) {
+
+        $(".contact-icons").hide();
+    }
+    else  {
+
+        $(".contact-icons").show();;
+    }
+});
+
+$(document).ready(function(){
+
+    if ((screen.width> 800)) {
+
+        $(".contact-i").show();s
+    }
+    else  {
+
+        $(".contact-i").hide();
+    }
+});
